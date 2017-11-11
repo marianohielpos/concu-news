5439743b05060fe7b36cc91edc4f35951a32fe7b
Seva Gavrilov <gavrilovseva@gmail.com>
2017-11-11 18:40:44 -0300

    Agregada una simple comunicación entre el cliente y el server.

--- src/procesos/Client.cpp
+++ src/procesos/Client.cpp
@@ -1,8 +1,25 @@
 #include "Client.h"
+#include "../ipcs/Cola.h"
+#include "protocol.h"
 
 #include <iostream>
 
 
 void Client::run() {
   std::cout << "Soy cliente" << std::endl;
+
+  Cola<message> cola("/bin/bash", 'a');
+
+  message m;
+  m.mtype = REQUEST;
+  m.type = TYPE_GET_CITY;
+
+  std::string key("quiero esto");
+  strcpy(m.key, key.c_str());
+
+  cola.escribir(m);
+  cola.leer(RESPONSE, &m);
+
+  std::cout << "Servidor me respondió " << m.value << std::endl;
+
 }
--- src/procesos/Server.cpp
+++ src/procesos/Server.cpp
@@ -2,6 +2,26 @@
 
 #include <iostream>
 
+#include "../ipcs/Cola.h"
+#include "protocol.h"
+
 void Server::run() {
   std::cout << "Soy server" << std::endl;
+
+  Cola<message> cola("/bin/bash", 'a');
+
+  message m;
+
+  cola.leer(REQUEST, &m);
+
+  std::cout << "Cliente me envió key: " << m.key << std::endl;
+
+  m.mtype = RESPONSE;
+  std::string value("respuestaza");
+  strcpy(m.value, value.c_str());
+  cola.escribir(m);
+
+  std::cout << "Terminandome! " << std::endl;
+
+  cola.destruir();
 }
